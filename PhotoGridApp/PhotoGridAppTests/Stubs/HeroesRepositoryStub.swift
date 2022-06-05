cbf49168b7a800f3345c2b72c397e2d655d7259f
Javier Heisecke <javierheiseckee@gmail.com>
2022-06-05 18:50:56 -0400

    adds unit tests

---
+++ PhotoGridApp/PhotoGridAppTests/Stubs/HeroesRepositoryStub.swift
@@ -0,0 +1,23 @@
+//
+//  HeroesRepositoryStub.swift
+//  PhotoGridAppTests
+//
+//  Created by Javier Heisecke on 2022-06-05.
+//
+
+import Foundation
+@testable import PhotoGridApp
+
+struct HeroesRepositoryStub: HeroesRepositoryProtocol {
+
+    var hasError = false
+
+    func getAllHeroes(success: @escaping (HeroesListResponse) -> Void, failure: @escaping (ApiError) -> Void) {
+        if hasError {
+            failure(ApiError(message: "Error"))
+        } else {
+            success(HeroesListResponse())
+        }
+    }
+
+}
